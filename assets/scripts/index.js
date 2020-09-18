588d32a433186aad2e2a2d7d274de44e20f1183e
Veniamin <Weniaminwelichko@ukr.net>
2020-09-18 14:43:12 +0300

    Fix commit

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,22 +1,26 @@
 'use strict';
 
-let regexpValidationPassword = new RegExp('^[A-Za-zА-Яа-я0-9]{8,20}$');
+let regexpValidation = new RegExp('^[A-Za-zА-Яа-я0-9_]{8,20}$');
 
-const input = document.getElementById('input');
 const form = document.getElementById('form');
+const input = document.getElementById('input');
+
+
 
 form.addEventListener('submit', (event)=>{
     event.preventDefault();
-    event.stopPropagation();
-});
 
-input.addEventListener('input', (event)=>{
-    const {currentTarget:{value}} = event;
-    
-    if(!value.match(regexpValidationPassword)){
-        event.currentTarget.style.border = '5px solid red';
+    const {currentTarget:{children:{input}}} = event;
+
+    if(regexpValidation.test(input.value)){
+        input.classList.toggle('defaultInputState');
+        input.classList.toggle('invalidInputState');
+        console.log('vavid');
     }
     else{
-        event.currentTarget.style.border = '';
+        input.classList.toggle('defaultInputState');
+        input.classList.toggle('validInputState');
+        console.log('Not vavid');
     }
+    
 });
